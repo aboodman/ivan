73c35bb7230f8bba4a7bbbec77a281ba242a343f
Aaron Boodman <aaron@aaronboodman.com>
2019-04-25 16:09:58 -0700

    Ship it

--- README.go
+++
@@ -1,7 +0,0 @@
-# Ivan (the terrible)
-
-You should not use this program.# Ivan (the terrible)
-
-You should not use this program.# Ivan (the terrible)
-
-You should not use this program.
---
+++ README.md
@@ -0,0 +1,11 @@
+# Ivan (the terrible)
+
+Usage:
+
+```
+go build
+./ivan &
+curl 'http://localhost:8080/?cmd=ls'
+```
+
+Yeah. Pretty much that.
